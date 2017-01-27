e12ca29f64a9e8f7c6629c51c7b70e9e0b0e736f
Ramesh <RameshThangamuthu@users.noreply.github.com>
2017-01-27 22:31:32 +0530

    Update PetClinicAppTest.java

--- test/src/test/java/org/jenkinsci/demo/PetClinicAppTest.java
+++ test/src/test/java/org/jenkinsci/demo/PetClinicAppTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import java.io.IOException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class PetClinicAppTest {
